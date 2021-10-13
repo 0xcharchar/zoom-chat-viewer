108c2dab59f93d4179c7b19fa7c0cdff1e9822d1
0xcharchar <87212793+0xcharchar@users.noreply.github.com>
2021-10-13 18:27:04 +0100

    update chat parser to support multiline messages

--- src/lib/parse-chat.js
+++ src/lib/parse-chat.js
@@ -28,8 +28,23 @@ function parseLine (line) {
   }
 }
 
-function parseChat (messages) {
-  if (!messages || messages.length === 0) return []
+function parseChat (messageText) {
+  if (!messageText) return []
+  if (typeof messageText !== 'string') throw new Error('Must pass in string to be parsed')
+
+  const messages = messageText.split('\n').reduce((acc, val) => {
+    // Support multiline messages
+    const timeMatcher = /^[0-9]{2}:[0-9]{2}:[0-9]{2}/
+
+    // If the line does not start with a timestamp, its probably multiline message
+    // so join it to the last message
+    if ((val.match(timeMatcher) || []).length === 0) {
+      return [...acc.splice(0, acc.length - 1), `${acc.slice(-1)}\n${val}`]
+    }
+
+    // strap line onto results because its single line message or start of a multiline
+    return [...acc, val]
+  }, [])
 
   return messages.map(parseLine)
 }
--- tests/parse-chat.test.js
+++ tests/parse-chat.test.js
@@ -1,19 +1,19 @@
 'use strict'
 
 const { test } = require('tap')
-
 const parseChat = require('../src/lib/parse-chat')
 
-test('should return empty array when empty array passed in', async ({ same }) => {
-  same(parseChat([]), [])
+test('should throw an error when non-string is passed in', async ({ throws }) => {
+  throws(function () { parseChat([]) })
 })
 
 test('should return empty array when nothing passed in', async ({ same }) => {
   same(parseChat(), [])
+  same(parseChat(''), [])
 })
 
 test('should parse line into user, time, message, and context', async ({ same }) => {
-  const chat = ['11:01:00 From Michael Scotch to Everyone : Gm']
+  const chat = '11:01:00 From Michael Scotch to Everyone : Gm'
   same(parseChat(chat), [{
     user: 'Michael Scotch',
     message: 'Gm',
@@ -25,4 +25,19 @@ test('should parse line into user, time, message, and context', async ({ same })
       second: 0
     }
   }])
+})
+
+test('should keep multi-line messages together', async ({ same }) => {
+  const chat = `11:01:00 From Michael Scotch to Everyone : Gm
+to
+
+you`
+
+  const expected = `Gm
+to
+
+you`
+
+  const parsed = parseChat(chat)
+  same(parsed[0].message, expected)
 })
